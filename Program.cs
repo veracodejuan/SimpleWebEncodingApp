8a7092c51d3aee9ad3e5769ff54faa25d57aef02
Vera Juan <veracodeJuan@gmail.com>
2024-08-14 00:09:21 +0000

    init commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace SimpleWebEncodingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example string that needs to be encoded
+            string input = "<script>alert('XSS');</script>";
+
+            // Encode the input using JavaScriptEncoder
+            string encodedInput = JavaScriptEncoder.Default.Encode(input);
+
+            // Display the encoded result
+            Console.WriteLine("Original: " + input);
+            Console.WriteLine("Encoded: " + encodedInput);
+
+            // Use the encoded string in a JSON object
+            var jsonObject = new { content = encodedInput };
+            string jsonString = JsonSerializer.Serialize(jsonObject);
+
+            Console.WriteLine("JSON with encoded content: " + jsonString);
+        }
+    }
+}
